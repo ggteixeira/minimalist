a59738a449a56770db2926c698b1abc49d11ceec
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-07-25 22:29:14 -0300

    Adds an incremental ID to the DTO (to solve the key warning in React)

--- minimalist-be/Minimalist/Data/Dtos/CreateTodoDto.cs
+++ minimalist-be/Minimalist/Data/Dtos/CreateTodoDto.cs
@@ -1,9 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Minimalist.Data.Dtos;
 
 public class CreateTodoDto
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id;
+
     [Required(ErrorMessage = "Todo title is required")]
     public string Title { get; set; }
 }
--- minimalist-be/Minimalist/Data/Dtos/ReadTodoDto.cs
+++ minimalist-be/Minimalist/Data/Dtos/ReadTodoDto.cs
@@ -2,6 +2,7 @@ namespace Minimalist.Data.Dtos;
 
 public class ReadTodoDto
 {
+    public string Id { get; set; }
     public string Title { get; set; }
     public DateTime ReadTime { get; set; } = DateTime.Now;
 }
