6b9f54e4df2492990d5a36279cd560be00b5eef4
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-07-26 13:39:13 -0300

    Updates .gitignore

---
+++ minimalist-be/Minimalist/Migrations/20250726155601_isCompleted Migration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minimalist.Migrations
+{
+    /// <inheritdoc />
+    public partial class isCompletedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isCompleted",
+                table: "Todos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isCompleted",
+                table: "Todos");
+        }
+    }
+}
