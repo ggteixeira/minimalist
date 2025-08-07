8a15449df0961936212cf7040fb69c9306646990
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-07 09:38:13 -0300

    Moves function to the custom hook

--- minimalist-fe/src/hooks/useFetchtodos.ts
+++ minimalist-fe/src/hooks/useFetchtodos.ts
@@ -1,5 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchTodos } from "../services/fetchTodos";
+
+import axios from "axios";
+
+const fetchTodos = async () => {
+  const PATH = "https://localhost:7071/todo";
+
+  const response = await axios.get(PATH);
+  return response.data;
+};
 
 export const useFetchTodos = () => {
   return useQuery({
--- minimalist-fe/src/services/fetchTodos.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchTodos = async () => {
-  const PATH = "https://localhost:7071/todo";
-
-  const response = await axios.get(PATH);
-  return response.data;
-};
