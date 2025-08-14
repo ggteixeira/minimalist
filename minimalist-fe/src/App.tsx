5aa94e7bb2113e7fd378cf27cadf23c3dafe15b9
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-14 11:50:41 -0300

    Adds react hoook form + integrates it with the mutation

--- minimalist-fe/src/App.tsx
+++ minimalist-fe/src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 import ListOfTodos from "./components/ListOfTodos";
 import { useAddTodo } from "./hooks/useAddtodo";
+import { useForm } from "react-hook-form";
 
 export default function App() {
-  const [newTodo, setNewTodo] = useState("");
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+    },
+  });
 
   const addTodoMutation = useAddTodo();
 
-  const handleAddTodo = () => {
-    addTodoMutation.mutate({ body: newTodo });
+  const handleAddTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addTodoMutation.mutate({ body: watch("title") });
   };
+
+  console.log(watch("title"));
 
   return (
     <div
@@ -22,19 +33,24 @@ export default function App() {
     >
       <h1>Todo List</h1>
       <div style={{ display: "flex", gap: "0.5rem", borderRadius: "8px" }}>
-        <input
-          style={{
-            border: "1px solid silver",
-            padding: "0.5rem",
-          }}
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Add new task"
-        />
-        <button disabled={newTodo.length === 0} onClick={handleAddTodo}>
-          Add
-        </button>
+        <form onSubmit={handleAddTodo}>
+          <input
+            style={{
+              border: "1px solid silver",
+              padding: "0.5rem",
+            }}
+            type="text"
+            value={watch("title")}
+            {...register("title")}
+            placeholder="Add new task"
+            required
+          />
+          {errors.title && <span>Please add a todo</span>}
+
+          <button type="submit" disabled={watch("title").length === 0}>
+            Add
+          </button>
+        </form>
       </div>
       <ListOfTodos />
     </div>
